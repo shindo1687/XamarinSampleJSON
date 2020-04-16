a4e4b25a80cc71091d148cc047cebd17bdce0b19
shindo <shindo1687@gmail.com>
2020-04-16 16:26:54 +0900

    初期コミット

---
+++ SampleJSON/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleJSON
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            String jsonData = GetJsonData("SampleJSON.Sample.json");
+            System.Console.WriteLine("jsonData = " + jsonData);
+
+            UserData userData = JsonSerializer.Deserialize<UserData>(jsonData);
+            System.Console.WriteLine("userData = " + userData);
+
+            String jsonString = JsonSerializer.Serialize(userData);
+            System.Console.WriteLine("jsonString = " + jsonString);
+        }
+
+        private String GetJsonData(String jsonFileName)
+        {
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream(jsonFileName);
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ SampleJSON/UserData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleJSON
+{
+    public class UserData
+    {
+        public string updated { get; set; }
+        public IList<User> users { get; set; }
+
+        public override string ToString()
+        {
+            string ret = "{updated=" + updated + ", users=[";
+            foreach(var user in users)
+            {
+                ret += user.ToString();
+            }
+            ret += "]}";
+            return ret;
+        }
+    }
+
+    public class User
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public bool delete { get; set; }
+
+        public override string ToString()
+        {
+            return "{name=" + name + ", age=" + age + ", delete=" + delete + "}";
+        }
+    }
+}
